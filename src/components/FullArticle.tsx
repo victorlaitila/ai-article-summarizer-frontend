951725ff7dab7faba819c1f1a34286a32329c1e9
Victor <viclait@gmail.com>
2025-10-04 15:17:05 +0300

    Full article structure fix

--- src/components/FullArticle.tsx
+++ src/components/FullArticle.tsx
@@ -3,7 +3,7 @@ import { CardContent } from "./ui/Card";
 export default function FullArticle({article}: {article: string}) {
   return (
     <CardContent>
-      <div className="bg-indigo-50 rounded-lg p-2.5">
+      <div className="bg-indigo-50 rounded-lg p-2.5 whitespace-pre-wrap">
         {article}
       </div>
     </CardContent>
